a7b895b496dd26fdb33f623e726426d75d21e869
Josh Bryson <josh@bryson.me.uk>
2020-03-20 00:07:27 +0000

    ES setup and inital Webpack install implemented

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,15 +1,13 @@
-namespace App {
-  // Project Type
-  export enum ProjectStatus { Active, Finished }
+// Project Type
+export enum ProjectStatus { Active, Finished }
 
-  // Project Type
-  export class Project {
-    constructor(
-      public id: string, 
-      public title: string,
-      public description: string, 
-      public people: number, 
-      public status: ProjectStatus
-      ) {}
-  }
+// Project Type
+export class Project {
+  constructor(
+    public id: string, 
+    public title: string,
+    public description: string, 
+    public people: number, 
+    public status: ProjectStatus
+  ) {}
 }
